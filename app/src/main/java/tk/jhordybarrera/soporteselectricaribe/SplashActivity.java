f28f67c5ce832911ebc84ea480877283fa8d0d8c
Jhordy Barrera <dyjhor93@gmail.com>
2020-05-07 00:09:14 -0500

    Little details

--- app/src/main/java/tk/jhordybarrera/soporteselectricaribe/SplashActivity.java
+++ app/src/main/java/tk/jhordybarrera/soporteselectricaribe/SplashActivity.java
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
-
+    Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        (new Handler()).postDelayed(this::lanzar, 2000);
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                startLogin(null);
+            }
+
+        }, 3000);   //5 seconds
+
     }
-    public void lanzar(){
+    public void startLogin(View v){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
-    };
+        handler.removeCallbacksAndMessages(null);
+    }
 }
