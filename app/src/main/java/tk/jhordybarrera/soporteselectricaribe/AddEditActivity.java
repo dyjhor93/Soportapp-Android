10324ab4a127519d75b5874b0fbb60f9d057e2de
Jhordy Barrera <dyjhor93@gmail.com>
2020-05-15 19:35:22 -0500

    Add camera and external SD support

--- app/src/main/java/tk/jhordybarrera/soporteselectricaribe/AddEditActivity.java
+++ app/src/main/java/tk/jhordybarrera/soporteselectricaribe/AddEditActivity.java
@@ -4,25 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddEditActivity extends AppCompatActivity {
     private EditText os;
     private EditText nic;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit);
         os = findViewById(R.id.clientOS);
         nic = findViewById(R.id.clientNic);
+        imageView = findViewById(R.id.gallery);
         load_data();
     }
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
     private void load_data() {
         if(getIntent().hasExtra("nic")){
             nic.setText(getIntent().getStringExtra("nic"));
@@ -42,7 +65,7 @@ public class AddEditActivity extends AppCompatActivity {
                 != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this,"No has consedido permisos de la camara",Toast.LENGTH_LONG).show();
         }else{
-            Toast.makeText(this,"Permiso de la camara concedido",Toast.LENGTH_LONG).show();
+            dispatchTakePictureIntent();
         }
     }
 }
