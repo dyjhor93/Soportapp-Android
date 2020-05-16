9558d651ac77bb56154b3faec9a9fb54d6e0fe3b
Jhordy Barrera <dyjhor93@gmail.com>
2020-05-16 12:45:34 -0500

    Request permission and publish APK

--- app/src/main/java/tk/jhordybarrera/soporteselectricaribe/AddEditActivity.java
+++ app/src/main/java/tk/jhordybarrera/soporteselectricaribe/AddEditActivity.java
@@ -2,6 +2,7 @@ package tk.jhordybarrera.soporteselectricaribe;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
@@ -82,7 +83,30 @@ public class AddEditActivity extends AppCompatActivity {
             os.setText(getIntent().getStringExtra("os"));
         }
     }
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA=1;
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    add_photo_video(null);
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this,this.getString(R.string.not_granted),Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
 
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
     public void add_photo_video(View v){
         //la idea es que se guarden en una carpeta que contenga el nic del usuario y dentro otra carpeta con la orden de servicio
         //app_data/nic/os/{os_img1.jpg, os_img2.jpg, os_video1.mp4, os_x.x}
@@ -91,7 +115,20 @@ public class AddEditActivity extends AppCompatActivity {
         //esto se controlara antes de subir al servidor
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this,this.getString(R.string.not_granted),Toast.LENGTH_LONG).show();
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},MY_PERMISSIONS_REQUEST_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
         }else{
             dispatchTakePictureIntent();
         }
